0dc163332fb472a7f10ec9e2b09dddf5261f412b
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2024-03-02 23:12:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"
+
+let a = "sachin"
+let b= 2
+let c = true
+let d;
+let e = null
+
+// Primitive Data types
+// number, bigint, string, boolean, null, undefined, symbol
+
+// Non-primitive datatypes
+// Object
+
+console.table([typeof a, typeof b, typeof c, typeof d, typeof e])
