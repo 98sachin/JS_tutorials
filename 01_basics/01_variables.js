44bab52c1408fe0505dd8f12e10394761b9cbfee
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2024-03-02 20:27:23 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const name = "sachin";
+let accountEmail = "asdf@asdf.com"
+var accPassword = "3840008"
+accountCity = "Mumbai"
+let accountState;
+
+console.log(name)
+
+accountEmail = "asdkfj@mn.com"
+accountCity="Bangalore"
+
+console.table([name, accPassword, accountEmail, accountCity, accountState])
