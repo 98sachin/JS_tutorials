bccfd015daa2d11ef92fee58510479f39c5600b4
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2024-03-02 23:37:04 +0000

    dates

---
+++ 01_basics/06_date_and_time.js
@@ -0,0 +1,14 @@
+// DATES
+// from 1 Jan 1970
+
+// let d = new Date();
+// console.log(d)
+// console.log(typeof d)
+// console.log(d.toString())
+// console.log(d.toDateString())
+// console.log(d.toLocaleString())
+// console.log(d.toLocaleDateString())
+
+// if you wish to create a specific date
+let myDate = new Date(2024, 8, 2)
+console.log(myDate.toString())
