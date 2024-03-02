de413d48c6664750b410810e30cb39e9d0be1146
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2024-03-02 21:21:23 +0000

    conversions and operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,88 @@
+// let score = "33";
+// let score1 = "44ab"
+// let score2 = null
+// let score3 = undefined
+// let score4 = true
+// console.log(typeof score);
+// console.log(typeof score1);
+// console.log(typeof score2);
+// console.log(typeof score3);
+// console.log(typeof score4);
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// let valueInNumber1 = Number(score1)
+// console.log(typeof valueInNumber1)
+// console.log(valueInNumber1)
+
+// let valueInNumber2 = Number(score2)
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2)
+
+// let valueInNumber3 = Number(score3)
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3)
+
+// let valueInNumber4 = Number(score4)
+// console.log(typeof valueInNumber4)
+// console.log(valueInNumber4)
+
+// let a = 'sachin'
+// let b = Number(a)
+// console.log(typeof b)
+// console.log( b)
+// 
+// let c = 'sachin'
+// let d = Boolean(c)
+// console.log(typeof d)
+// console.log(d)
+
+// *************************OPERATIONS*******************************
+
+    // console.log(2*3)
+    // console.log(2+3)
+    // console.log(2-3)
+    // console.log(2/3)
+    // console.log(2%3)
+    // console.log(2**3)
+    
+
+// IMPORTANT OPERATIONS
+
+// let val = 2
+// let negativeVal = -val
+// console.log(negativeVal)
+
+// let str1 = 'sachin'
+// let str2 = ' sharma'
+// console.log(str1+str2)
+
+// let a = console.log("1"+2)
+// let b = console.log(1+"2")
+// let c = console.log("1"+2+2)
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+
+// let d = console.log(1+2+"2")
+// console.log(typeof d)
+
+// let e = console.log("3"+ +3)
+// console.log(typeof e)
+
+// let f = console.log(3+ +"3")
+// console.log(typeof f)
+
+// let g = console.log(3+3-3)
+// console.log(typeof g)
+
+// let h = console.log("3"+"3"-"3")
+// console.log(typeof h)
+
+
+console.log(true)
+console.log(+true)
+console.log(-true)
+console.log(+"")
