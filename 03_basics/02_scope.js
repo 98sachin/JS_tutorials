d25c65fef831efa53dc3f56743f61c295538d79e
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2024-03-04 21:17:25 +0000

    scope2

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -8,3 +8,46 @@ let c =30
 // console.log(c)
 
 // watch the video
+
+// =======================================================================================================
+
+// nested scope
+
+// function one(){
+//     const username = "sachin"
+    
+//     function two(){
+//         const website = "youtube.com"
+//         console.log(username)
+//     }
+//     // console.log(website) // this gives an error because it is out of scope
+//     // two()
+// }
+// one()
+
+// the above example also explains about closure i.e., the inner function can access the outer function.
+
+// understanding the scope concept in if/else statement
+// if(true){
+//     const username = "sachin"
+//     if(username === "sachin"){
+//         const website = " wikipedia.com"
+//         console.log(username+website);
+    // }
+    // console.log(website) // gives an error
+// }
+// console.log(username) // gives an error
+
+// ================================================================
+// console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+// console.log(addOne(5))
+
+
+console.log(addtwo)
+const addtwo = function(num2){
+    return num2+1
+}
+console.log(addtwo)
