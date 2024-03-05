79eb6238ab412663703d3f467877c44e773bcdb1
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2024-03-05 06:33:26 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,86 @@
+// syntax of switch case:
+// switch (key){  // key is the value that we want to check 
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month =9;
+// switch (month){
+//     case 1:
+//         console.log("January");
+//         break;  
+//     case 2:
+//         console.log("February");
+//         break;  
+//     case 3:
+//         console.log("March");
+//         break;  
+//     case 4:
+//         console.log("April");
+//         break;  
+//     case 5:
+//         console.log("May");
+//         break;  
+//     case 6:
+//         console.log("June");
+//         break;  
+//     case 7:
+//         console.log("July");
+//         break;  
+//     case 8:
+//         console.log("August");
+//         break;  
+//     case 9:
+//         console.log("September");
+//         break;  
+//     case 10:
+//         console.log("October");
+//         break;  
+//     case 11:
+//         console.log("November");
+//         break;  
+//     case 12:
+//         console.log("December");
+//         break; 
+        
+//     default:
+//         console.log("default case matched");
+//         break;
+// }
+
+// by default when a case is matched , then all the code below the matched case will be executed except default. i.e., why we use break.
+// if none of the case is matched , default case will be executed.
+
+// const days = "sunday";
+// switch (days){
+//     case "monday":
+//         console.log(`it's Monday.`)
+//         break;
+//     case "tuesday":
+//         console.log(`it's Tuesday.`)
+//         break;
+//     case "wednesday":
+//         console.log(`it's Wednesday.`)
+//         break;
+//     case "thursday":
+//         console.log(`it's Thursday.`)
+//         break;
+//     case "friday":
+//         console.log(`it's Friday.`)
+//         break;
+//     case "saturday":
+//         console.log(`it's Saturday.`)
+//         break;
+//     case "sunday":
+//         console.log(`it's Sunday.`)
+//         break;
+//     default:
+//         console.log(`invalid input.`);
+//         break;
+// }
+
+
