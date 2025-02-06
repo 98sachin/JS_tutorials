2df2be65c86a7469f72b1ace968e0dd15c785a25
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2025-02-06 14:27:09 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,5 @@
-const name = "sachin";
+const name = "sachin"; // a const variable cannot be uninitialzed ie., a const variable once declare has to be initialized ( assign a value )
+// const name1; // this will throw an error.
 let accountEmail = "asdf@asdf.com"
 var accPassword = "3840008"
 accountCity = "Mumbai"
