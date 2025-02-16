9cfaba57b287f4b5ecdb2334dbc794f815d6b467
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2025-02-17 04:37:05 +0530

    Update 01_loops.js

--- 05_iterations/01_loops.js
+++ 05_iterations/01_loops.js
@@ -76,3 +76,72 @@ do{
     console.log(`the score is ${score}`);
     score++
 }while(score<=10);  
+
+// =============================================================================================================================================
+
+//  for-each loop
+
+const coding = ["html", "css", "javascript", "react", "nodejs", "express"];
+
+const values = coding.forEach((item) => {
+  // console.log(item); // try printing this code with and without commenting this line
+  return item; // try printing this code with and without commenting this line
+});
+// console.log(values);
+
+// the result will always have an undefined value because the forEach loop does not return anything.
+
+// Example 1:
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// understanding the filter method
+// 1.
+// let newNums = myNums.filter((num) => num > 4);
+// 2.
+// let newNums = myNums.filter((num) => {
+//   return num > 4;
+// });
+
+// the above two examples are the same, the second one is just a bit more detailed with the curly braces and the return keyword. The first one is a shorthand version of the second one. Both will give the same result. Check by printng the second eg withou the return keyword.
+// The thing to remember is that whenever we use the curly braces i.e., scope, we have to use the return keyword to return the value.
+
+// Using for-each loop to filter the numbers greater than 4
+const newNums = [];
+myNums.forEach((num) => {
+  if (num > 4) {
+    newNums.push(num);
+  }
+});
+
+// console.log(newNums);
+
+// Example 2:
+
+const books = [
+  { title: "The Alchemist", author: "Paulo Coelho", genre: "Adventure" },
+  { title: "1984", author: "George Orwell", genre: "Dystopian" },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "Fiction",
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Sal inger",
+    genre: "Fiction",
+  },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Fiction" },
+  { title: "The Hobbit", author: "J.R.R. Tolkien", genre: "Fantasy" },
+  {
+    title: "The Lord of the Rings",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+  },
+  { title: "The Da Vinci Code", author: "Dan Brown", genre: "Mystery" },
+];
+// books is an array of objects
+// we want to get the titles of the books that are in the fiction genre
+// we can use the filter method to achieve this
+const userBooks = books.filter((book) => book.genre === "Fiction");
+console.log(userBooks);
